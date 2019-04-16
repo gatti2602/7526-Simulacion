dff67c3750ddf51716c3bc3c24b0e3e20b3f6b65
german-rotili <german.rotili@gmail.com>
2019-04-16 19:46:14 -0300

    Ejercicio 1

---
+++ tp1/1.py
@@ -0,0 +1,32 @@
+"""
+Ejercicio 1
+"""
+import numpy as np
+import matplotlib.pyplot as mp
+import random
+import math
+
+
+class GCL():
+
+    def __init__(self, current = 0):
+        self.current = current
+
+    def random(self, seed):
+        self.current = round(((1013904223 * seed) + 1664525) % (2 ^ 32))
+        return self.current / (2^32)
+
+    def next(self):
+        number = self.random(self.current)
+        return number
+
+seed = 97198
+generator = GCL(seed)
+total_samples = 100000
+numbers = []
+for i in range(0, total_samples):
+	numbers.append(generator.next())
+    #print(generator.next())
+
+mp.hist(numbers,100)
+mp.savefig("1-hist.png")
