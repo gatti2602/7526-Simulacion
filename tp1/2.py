5e0042dea28ae9c919c5fb7936a8f831cf4375b2
Juan Orlando <jm.orlando.ml@gmail.com>
2019-04-24 10:59:04 -0300

    ej 2

--- tp1/2.py
+++ tp1/2.py
@@ -5,15 +5,30 @@ import numpy as np
 import matplotlib.pyplot as mp
 import random
 
+class GCL():
+	def __init__(self, seed = 0):
+		self.current = seed
+
+	def random(self):
+		self.current = round(((1013904223 * self.current) + 1664525) % (2**32))
+		return self.current / (2**32)
+
+	def next(self):
+		number = self.random()
+		return number
+
 def inverse(a):
 	lamb = 20
 	result = (-1/lamb) * np.log(1-a)
 	return result
 
+seed = 93152 * 0.15 + 93570 * 0.25 + 99722 * 0.6
+generator = GCL(seed)
+
 total_samples = 100000
 numbers = []
 for i in range(0, total_samples):
-	numbers.append(inverse(random.uniform(0,1)))
+	numbers.append(inverse(generator.next()))
 
 l = np.array(numbers)
 print("Media obtenida: ", round(l.mean(), 4))
