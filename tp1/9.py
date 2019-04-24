2e59071b1196ec2a2188d9d76cd2a881750cfd1c
gatti2602 <gatti2602@gmail.com>
2019-04-24 10:16:18 -0300

    ej9 fixed

--- tp1/9.py
+++ tp1/9.py
@@ -12,16 +12,15 @@ import scipy.stats
 import math as m
 
 class GCL():
+	def __init__(self, seed = 0):
+		self.current = seed
 
-	def __init__(self, current = 0):
-		self.current = current
-
-	def random(self, seed):
-		self.current = round(((1013904223 * seed) + 1664525) % (2 ^ 32))
-		return self.current / (2^32)
+	def random(self):
+		self.current = round(((1013904223 * self.current) + 1664525) % (2**32))
+		return self.current / (2**32)
 
 	def next(self):
-		number = self.random(self.current)
+		number = self.random()
 		return number
 
 def gap_test(samples, a, b, t):
@@ -31,7 +30,7 @@ def gap_test(samples, a, b, t):
 	while s != n and j != len(samples):
 		j, r = j+1, 0
 		while j != len(samples) and not (a < samples[j] < b ):
-			j, r = j+1, min(r+1, t)
+			j, r = j+1, min(r+1, t-1)
 		#print(j, r)
 		count[r] += 1
 		s += 1
@@ -41,30 +40,29 @@ def gap_test(samples, a, b, t):
 	expected = t*[0]
 	p = b - a
 	expected[t-1] = ((1-p)**t) * n
-
 	for i in range(0, t-1):
 		expected[i] = (p*(1-p)**i) * n
 	#print("Sample", samples)
+	expected = [round(e) for e in expected]
 	print ("------------------")
 	print("Samples %d" % len(samples))
 	print("Count:", count)
-	print("Expected:", [round(e,2) for e in expected])
-	s, p = scipy.stats.chisquare(count, expected)
-
-	return s, p	
+	print("Expected:", expected)
+	s, pv = scipy.stats.chisquare(count, f_exp=[round(e) for e in expected])
+	print("p-value: %f" % pv)
+	print("s: %f" % s)
+	print("t was %d" % t)
+	return s, pv
 
 def main():
-	for i in range(3, 8):
-		for t in range(5, 20):
-			seed = 93570
-			generator = GCL(seed)
-			total_samples = 10**i
-			numbers = []
-			for i in range(0, total_samples):
-				numbers.append(generator.next())
-			s, p = gap_test(numbers, 0.2, 0.5, t)
-			print("p-value: %d" % p)
-			print("t was %d" % t)
+	seed = 93152 * 0.15 + 93570 * 0.25 + 99722 * 0.6
+	generator = GCL(seed)
+	total_samples = 10**5
+	t = 20
+	numbers = []
+	for i in range(0, total_samples):
+		numbers.append(generator.next())
+	s, p = gap_test(numbers, 0.2, 0.5, t)
 		
 main()		
 		
