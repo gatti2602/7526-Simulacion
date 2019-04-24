14f6feeace2764a043e2c4040f32069415b68486
Juan Orlando <jm.orlando.ml@gmail.com>
2019-04-24 11:04:55 -0300

    ej 5

--- tp1/5.py
+++ tp1/5.py
@@ -6,6 +6,19 @@ import matplotlib.pyplot as mp
 import random
 import collections
 
+class GCL():
+	def __init__(self, seed = 0):
+		self.current = seed
+
+	def random(self):
+		self.current = round(((1013904223 * self.current) + 1664525) % (2**32))
+		return self.current / (2**32)
+
+	def next(self):
+		number = self.random()
+		return number
+
+
 def empiric(a):
 	if a<=0.4:
 		return 1
@@ -18,10 +31,12 @@ def empiric(a):
 	else:
 		return 5
 
+seed = 93152 * 0.15 + 93570 * 0.25 + 99722 * 0.6
+generator = GCL(seed)
 total_samples = 100000
 numbers = []
 for i in range(0, total_samples):
-	numbers.append(empiric(random.uniform(0,1)))
+	numbers.append(empiric(generator.next()))
 
 cant = collections.Counter(numbers)
 print(cant)  
